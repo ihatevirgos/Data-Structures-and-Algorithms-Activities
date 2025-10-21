f624cb047bfbd8d3c4e6cba60d61126ee916a169
adi <bantogsalahadin@gmail.com>
2025-10-22 00:47:20 +0800

    add some comments

--- Activity4/activity4_item1.c
+++ Activity4/activity4_item1.c
@@ -31,6 +31,8 @@ struct node* addLinkedList(struct node* list1, struct node* list2){
 
     //loop runs while there's still a digit or a carry
     while (list1 != NULL || list2 != NULL || carry != 0){
+
+        //gets the value of the digit
         int digitList1 = (list1 != NULL) ? list1 -> data : 0;
         int digitList2 = (list2 != NULL) ? list2 -> data : 0;
 
@@ -40,6 +42,7 @@ struct node* addLinkedList(struct node* list1, struct node* list2){
         current -> link = newNode(sum % 10);
         current = current -> link;
 
+        //moves to the next digit
         if (list1 != NULL)
             list1 = list1 -> link;
         if (list2 != NULL)
@@ -63,7 +66,6 @@ struct node* stringConvert(char number[]){
     struct node* head = NULL;
     int length = strlen(number);
 
-    //turn list in reverse
     for (int index = 0; index < length; index++){
         if (number[index] >= '0' && number[index] <= '9')
             insertAtBeginning(&head, number[index] - '0');
@@ -81,8 +83,8 @@ struct node* reverseList(struct node* head){
     while (current != NULL){
         link = current -> link; //stores next node
         current -> link = prev; //reverse the link
-        prev = current; // move the prev forward
-        current = link; // move the current forward
+        prev = current; //move the prev forward
+        current = link; //move the current forward
     }
 
     return prev; //new head (prev tail)
@@ -109,6 +111,7 @@ int main(){
 
     printf("%s + %s = ", number1, number2);
     printList(result);
+
 
 
     return 0;
